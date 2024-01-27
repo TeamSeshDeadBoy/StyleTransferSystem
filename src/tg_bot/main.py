61e5c38d9fc4a0b3ab44ea8467c67f480e54c48f
StepanLebedevu <stepan_leb@mail.ru>
2024-01-27 20:38:09 +0300

    Bot connected with jobs

--- src/tg_bot/main.py
+++ src/tg_bot/main.py
@@ -1,9 +1,13 @@
 import telebot
 from dotenv.main import load_dotenv
 import os
+import requests
+import time
+import base64
 
 load_dotenv()
 api_token = os.environ["API_TOKEN_TELEGRAM"]
+url = os.environ["BACKEND_ADRESS"]
 
 bot = telebot.TeleBot(api_token, parse_mode="HTML")
 
@@ -38,5 +42,34 @@ def get_broadcast_picture(message):
         print("Попытка повторного создания папки пользователя")
     with open('./data/user_' + user_id + '/' + str(file_path.split('/')[1]), "wb") as new_file:
         new_file.write(file)
+        
+        
+@bot.message_handler(commands=['done'])
+def job_process(message):  
+    user_id = str(message.from_user.id)  
+    initial_job_responce = requests.get(url+"add_job/picture/user_"+user_id)
+    print(initial_job_responce.json())
+    
+    job_id = initial_job_responce.json()['job_id']
+    
+    status_response = requests.get(url+"job_status/"+job_id)
+    job_status = status_response.json()['job_status']
+    print("Status of job",job_id,":", job_status)
+    max_fetches = 0
+    while job_status != "finished" and max_fetches < 30:
+        status_response = requests.get(url+"job_status/"+job_id)
+        job_status = status_response.json()['job_status']
+        max_fetches += 1
+        print("Status of job",job_id,":", job_status)
+        time.sleep(5)
+    if job_status == "finished":
+        result_responce = requests.get(url+"job_result/"+job_id)
+        result = result_responce.json()['job_result']
+        print('Job', job_id, "result:", result)
+        bot.send_message(message.from_user.id, "Обработка фото успешно завершена")
+        images = [telebot.types.InputMediaPhoto(base64.b64decode(img)) for img in result]
+        bot.send_media_group(message.from_user.id, images)
+    
+    
  
-bot.infinity_polling()
+bot.polling(none_stop=True)
