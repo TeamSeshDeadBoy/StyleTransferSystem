bec5cfc10a4aff87e3a1a63f9dab1538da782c02
StepanLebedevu <stepan_leb@mail.ru>
2024-02-01 10:38:56 +0300

    Model downloading in mounted volume

---
+++ consumer/check.sh
@@ -0,0 +1,12 @@
+ if [ -d $data/models/models--TencentARC--PhotoMaker ]
+ then
+     echo "Model pre-installed"
+ else
+     echo "Installing model"
+     python3 -c 'import os; os.environ["HF_HOME"] = "../data/models"; os.environ["TRANSFORMERS_CACHE"] = "../data/models"; from huggingface_hub import hf_hub_download; downloaded_model_path = hf_hub_download(
+                                                          repo_id="TencentARC/PhotoMaker",
+                                                          filename="photomaker-v1.bin",
+                                                          repo_type="model",
+                                                          cache_dir="../data/models"
+                                                         ); print(downloaded_model_path)'
+ fi
